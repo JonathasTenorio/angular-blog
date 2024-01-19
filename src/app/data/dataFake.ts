955299227e94e03808d5aed4e41b3bba996a66c7
Jonathas Gabriel Davide Tenorio <2009641@aluno.univesp.br>
2024-01-18 22:07:59 -0300

    criação de content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"FSOCIETY",
+        "description":"WE ARE FSOCIETY",
+        "photoCover":"https://www.webassetscdn.com/avira/prod-blog/wp-content/uploads/2016/08/avira_blog_mr.robot-header-360x270.jpg"
+    },
+    {
+        "id":"2",
+        "title":"saiu novo filme",
+        "description":"ola",
+        "photoCover":""
+    },
+]
